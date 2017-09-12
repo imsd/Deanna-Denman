eee34e824e599ba10795687a32b6e37f0e24dd2f
DeannaDash <student@ent-v314-005.local>
2017-09-12 15:46:23 -0400

    tuesday week 3

---
+++ DayOne/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	//it will show up in the inspector 
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0, 360, 0) * Time.deltaTime);
+	}
+}
