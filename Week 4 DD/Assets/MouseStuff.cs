1cda8bf28dc2556cba622648b43ba42f60c2770a
DeannaDash <student@ent-v314-005.local>
2017-09-26 15:46:35 -0400

    Week5

--- Week 4 DD/Assets/MouseStuff.cs
+++ Week 4 DD/Assets/MouseStuff.cs
@@ -2,18 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Input : MonoBehaviour {
+public class MouseStuff : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+		
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
-		float rotation = Input.GetAxis ("Horizontal") * multiplier;
-		Vector3 v3 = new Vector3 (rotation, 0, 0);
-
-		transform.Rotate (v3);
+		
 	}
 }
