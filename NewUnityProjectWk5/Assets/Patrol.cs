87bc0adf9318f7d6c0cffe65377c6de3d72d5e0b
DeannaDash <student@ent-v314-005.local>
2017-09-28 15:47:05 -0400

    UnityProjectWeek5

---
+++ NewUnityProjectWk5/Assets/Patrol.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+	public class Patrol : MonoBehaviour  {
+
+		private bool dirRight = true;
+		public float speed = 2.0f;
+	void Start () {
+
+		InvokeRepeating("ChangeDirection", 0, 2);
+	}
+		void Update () {
+		if (dirRight)
+			transform.Translate (Vector3.right * speed * Time.deltaTime);
+		else
+			transform.Translate (Vector3.right * -1 * speed * Time.deltaTime);
+
+	
+	} 
+	void ChangeDirection() {
+		
+		dirRight = !dirRight;
+
+	}
+}
