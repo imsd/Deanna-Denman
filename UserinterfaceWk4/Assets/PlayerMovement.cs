6699ec4be40cf3ec0c64d5b8b1d6ce223095f344
DeannaDash <Deannadenman19@gmail.com>
2017-09-28 01:03:55 -0400

    Week4HW

---
+++ UserinterfaceWk4/Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float moveSpeed;
+	private float maxSpeed;
+
+	private Vector3 input;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		input = new Vector3(Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+		GetComponent<Rigidbody>().AddForce(input * moveSpeed);
+	}
+}
---
+++ UserinterfaceWk4/Assets/explode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explode : MonoBehaviour {
+
+	public GameObject explosion; // drag your explosion prefab here
+
+	void OnCollisionEnter(){
+		GameObject expl = Instantiate(explosion, transform.position, Quaternion.identity) as GameObject;
+		Destroy(gameObject); // destroy the box
+		Destroy(expl, 3); // delete the explosion after 3 seconds
+	// Use this for initialization
+
+
+
+		}
+	}
+	
+	// Update is called once per frame
+
+		
+	
