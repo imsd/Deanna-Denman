add6c33e4460e85baa6602bdd045694eef4e4b89
DeannaDash <student@ent-v314-005.local>
2017-12-19 15:51:09 -0500

    final project

---
+++ ConsumptionProject2ndTry/Assets/HoverText.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverText : MonoBehaviour {
+
+	public string HoverTxt1;
+	public string HoverTxt2;
+	public string HoverTxt3;
+	public TextMesh DisplayTextObject;
+
+	void Update() {
+
+		if (Time.time > 10f) {
+			// switch the 3d text so that its text value
+			// is now whatever text value this hover instance holds
+			DisplayTextObject.text = HoverTxt1;
+
+			// show it
+			DisplayTextObject.color = Color.white;
+		}
+
+		if (Time.time > 20f) {
+			// switch the 3d text so that its text value
+			// is now whatever text value this hover instance holds
+			DisplayTextObject.text = HoverTxt2;
+
+			// show it
+			DisplayTextObject.color = Color.white;
+		}
+		if (Time.time > 30f) {
+			// switch the 3d text so that its text value
+			// is now whatever text value this hover instance holds
+			DisplayTextObject.text = HoverTxt3;
+
+			// show it
+			DisplayTextObject.color = Color.white;
+		}
+
+	}
+
+	void OnMouseExit() {
+		DisplayTextObject.color = new Color(0,0,0,0);
+	}
+}
---
+++ ConsumptionProject2ndTry/Assets/NewHover.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewHover : MonoBehaviour {
+
+	public string HoverTxt4;
+	public string HoverTxt5;
+	public string HoverTxt6;
+	public string HoverTxt7;
+
+	public TextMesh DisplayTextObject;
+
+	void Update() {
+
+		if (Time.time > 15f) {
+			// switch the 3d text so that its text value
+			// is now whatever text value this hover instance holds
+			DisplayTextObject.text = HoverTxt4;
+
+			// show it
+			DisplayTextObject.color = Color.green;
+		}
+
+		if (Time.time > 25f) {
+			// switch the 3d text so that its text value
+			// is now whatever text value this hover instance holds
+			DisplayTextObject.text = HoverTxt5;
+
+			// show it
+			DisplayTextObject.color = Color.green;
+		}
+		if (Time.time > 35f) {
+			// switch the 3d text so that its text value
+			// is now whatever text value this hover instance holds
+			DisplayTextObject.text = HoverTxt6;
+
+			// show it
+			DisplayTextObject.color = Color.green;
+		}
+	
+	}
+
+	void OnMouseExit() {
+		DisplayTextObject.color = new Color(0,0,0,0);
+	}
+}
